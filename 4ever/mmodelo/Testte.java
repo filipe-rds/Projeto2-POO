889cc9776e009dff5fadc5d2b072ec47729be0b9
Gabriel Félix <gfedacs@hotmail.com>
2023-11-28 22:29:25 -0300

    second commit
    add salvarObjetos()

--- 4ever/mmodelo/Testte.java
+++ 4ever/mmodelo/Testte.java
@@ -1,6 +1,6 @@
 package mmodelo;
 import java.time.LocalDate;
-
+import repositorio.Repositorio;
 public class Testte {
 
 	public static void main(String[] args) {
@@ -8,7 +8,28 @@ public class Testte {
 		// TODO Auto-generated method stub
 		
 		Convidado gabriel = new Convidado("123",LocalDate.of(1900,5, 15),"Google");
+
+		Repositorio r1 = new Repositorio();
+
+		LocalDate dataExemplo = LocalDate.of(2023, 11, 28);
+		Evento e1 = new Evento (dataExemplo,"qwqhbqb",18,20.0);
+		LocalDate dataExemplo1 = LocalDate.of(2023, 11, 28);
+		Evento e2 = new Evento (dataExemplo1,"qgjgjqvdukqwqeqwwqeqww",18,20.0);
+
+		LocalDate dataExemplo3 = LocalDate.of(2023, 10, 15);
+		Participante p1 = new Participante ("123",dataExemplo3);
+
+		LocalDate dataExemplo4 = LocalDate.of(2023, 10, 15);
+		Convidado c1 = new Convidado ("12312412",dataExemplo4,"Google");
+
+		r1.adicionar(p1);
+		r1.adicionar(c1);
+		r1.adicionar(e1);
+		r1.salvarObjetos();
+		r1.adicionar(e2);
+		r1.salvarObjetos();
 		
+
 		System.out.println(gabriel);
 		
 	}
--- 4ever/repositorio/Repositorio.java
+++ 4ever/repositorio/Repositorio.java
@@ -1,6 +1,12 @@
 package repositorio;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import mmodelo.Evento;
+import java.util.Scanner;
+
+import mmodelo.Convidado;
+import mmodelo.Evento; 
 import mmodelo.Ingresso;
 import mmodelo.Participante;
 
@@ -81,6 +87,97 @@ public class Repositorio {
 		return eventos.size() +1;
 		
 	}
+
+	public void carregarObjetos()  	{
+		// carregar para o repositorio os objetos dos arquivos csv
+		try {
+			//caso os arquivos nao existam, serao criados vazios
+			File f1 = new File( new File(".\\eventos.csv").getCanonicalPath() ) ; 
+			File f2 = new File( new File(".\\participantes.csv").getCanonicalPath() ) ; 
+			File f3 = new File( new File(".\\ingressos.csv").getCanonicalPath() ) ; 
+			if (!f1.exists() || !f2.exists() || !f3.exists() ) {
+				//System.out.println("criando arquivo .csv vazio");
+				FileWriter arquivo1 = new FileWriter(f1); arquivo1.close();
+				FileWriter arquivo2 = new FileWriter(f2); arquivo2.close();
+				FileWriter arquivo3 = new FileWriter(f3); arquivo3.close();
+				return;
+			}
+		}
+		catch(Exception ex)		{
+			throw new RuntimeException("criacao dos arquivos vazios:"+ex.getMessage());
+		}
+
+		String linha;	
+		String[] partes;
+
+
+	}
+
+	public void	salvarObjetos()  {
+		//gravar nos arquivos csv os objetos que estão no repositório
+		try	{
+			File f = new File( new File(".\\eventos.csv").getCanonicalPath())  ;
+			FileWriter arquivo1 = new FileWriter(f);
+
+			
+			for(Evento e : eventos){
+
+				String dataFormatada = e.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+				arquivo1.write(	
+								e.getId()+";"+
+								dataFormatada +";"+
+								e.getDescricao()+";"+
+								e.getCapacidade()+";"+
+								e.getPreco() +
+								"\n");	
+			}
+			arquivo1.close();
+		}
+
+		catch(Exception e){
+			throw new RuntimeException("problema na criação do arquivo  eventos "+e.getMessage());
+		}
+
+		try	{
+			File f = new File( new File(".\\participantes.csv").getCanonicalPath())  ;
+			FileWriter arquivo2 = new FileWriter(f);
+
+			
+			for(Participante p : participantes){
+
+				if(p instanceof Convidado){
+
+				Convidado c = (Convidado) p;
+
+				String dataFormatada = p.getNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				arquivo2.write(	
+								c.getCpf() + ";" +
+								dataFormatada + ";" +
+								c.getEmpresa() +
+						"\n");	
+			}
+			else {
+				String dataFormatada = p.getNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				arquivo2.write(	
+								p.getCpf() + ";" +
+								dataFormatada + ";" + "\n");	
+
+			}
+		}
+
+			arquivo2.close();
+		}
+
+		catch(Exception e){
+			throw new RuntimeException("problema na criação do arquivo  eventos "+e.getMessage());
+		}
+
+
+
+	}
+}
+	
 	
 
-}
+
